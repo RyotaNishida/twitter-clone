5e85d4a431f87682c31e6c361625470d5b10f50b
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-03 18:29:00 +0900

    ツイート機能のルーティングを追加

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/tweet', [App\Http\Controllers\TweetController::class, 'tweet']);
+Route::post('/tweet', [App\Http\Controllers\TweetController::class, 'postTweet']);
