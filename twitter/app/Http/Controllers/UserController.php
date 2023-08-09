8301cf4f71b4c16bedc29cab0012e30081d5e604
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-09 15:04:36 +0900

    [edit]ユーザーコントローラーの修正

--- twitter/app/Http/Controllers/UserController.php
+++ twitter/app/Http/Controllers/UserController.php
@@ -3,51 +3,62 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Requests\UserUpdateRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class UserController extends Controller
 {
     /**
-     * ユーザー詳細画面を表示
+     * ユーザーIDを取得
      *
-     * @param string $userId
+     * @param integer $userId
      * @param User $user
      * @return View|RedirectResponse
      */
-    public function findByUserId(string $userId, User $user): View|RedirectResponse
+    public function findByUserId(int $userId, User $user): User
     {
         //ログイン中のIDを取得
         if(Auth::id() !== (int) $userId) {
             return redirect()->route('home');
         }
-        $userDetail = $user->findByUserId($userId);
+        return $user->findByUserId($userId);
+    }
 
+
+    /**
+     * ユーザー詳細画面を表示
+     *
+     * @param integer $userId
+     * @param User $user
+     * @return View
+     */
+    public function show(int $userId, User $user): View
+    {
+        $userDetail = $this->findByUserId($userId, $user);
         return view('user.show', ['userDetail' => $userDetail]);
     }
+
 
     /**
      * ユーザー情報の更新
      *
-     * @param Request $request
-     * @param string $userId
+     * @param UserUpdateRequest $request
      * @return RedirectResponse
      */
-    public function userUpdate(Request $request): RedirectResponse
+    public function update(UserUpdateRequest $request): RedirectResponse
     {
         $user = User::findOrFail(Auth::id());
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        
+        if($request->hasAnyErrors()) {
+            return redirect()->back()->withErrors($request->errors())->withInput();
+        }
         $data = [
             'name' => $request->input('name'),
             'email' => $request->input('email'),
         ];
 
-        $user->userUpdate($data);
-
+        $user->update($data);
         return redirect()->route('home')
             ->with('success', 'ユーザー情報が更新されました！');
     }
@@ -58,17 +69,14 @@ class UserController extends Controller
      * @param string $userId
      * @return RedirectResponse
      */
-    public function userDelete(string $userId): RedirectResponse
+    public function delete(string $userId): RedirectResponse
     {
         $user = User::find($userId);
-        
+        $user->userDelete($userId);
+
         if($user) {
-            $user->delete();
-            session()->flash('success', 'ユーザーが削除されました');
-        } else {
-            session()->flash('error', 'ユーザーが見つかりませんでした');
+            return redirect()->route('home');
         }
-        return redirect()->route('home');
     }
 
     /**
@@ -78,7 +86,6 @@ class UserController extends Controller
      */
     public function getAll(): View
     {
-        $users = User::all();
-        return view('user.index', ['users' => $users]);
+        return view('user.index', ['users' => User::all()]);
     }
 }
