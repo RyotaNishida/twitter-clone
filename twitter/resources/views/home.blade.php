bde5b4acd3a54f5a496b24ffd0bed88c74afe681
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-13 23:38:41 +0900

    [edit]ホーム画面・ヘッダーの編集

--- twitter/resources/views/home.blade.php
+++ twitter/resources/views/home.blade.php
@@ -3,27 +3,37 @@
 @section('title', 'ホーム')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class='container'>
+        <div class='row justify-content-center'>
+            <div class='col-md-8'>
+                <div class='card'>
+                    <div class='card-header'>{{ __('Dashboard') }}</div>
+                    <div class='card-body'>
+                        @if (session('status'))
+                            <div class='alert alert-success' role='alert'>
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        @if (session('success'))
+                            <div>
+                                {{ session('success') }}
+                            </div>
+                        @endif
+                        {{ __('You are logged in!') }}
+                    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if(session('success'))
-                        <div>
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
+                    <div>
+                        <a href="{{ route('tweet.create') }}">
+                            ツイートする
+                        </a>
+                    </div>
+                    <div>
+                        <a href="{{ route('tweet.getAll') }}">
+                            ツイート一覧
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
