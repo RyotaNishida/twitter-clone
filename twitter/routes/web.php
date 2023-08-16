08a914ea36124a8410f6ac8e4c0f7c7c0e64586f
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-16 14:36:13 +0900

    [update]ルーティングの追加

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -22,10 +22,16 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group(['middleware' => 'auth'], function() {
     Route::group(['prefix' => 'user', 'as' => 'user.'], function() {
-        Route::get('/', [App\Http\Controllers\UserController::class, 'getAll'])->name('index');
         Route::get('/{id}', [App\Http\Controllers\UserController::class, 'show'])->name('show');
         Route::put('/{id}', [App\Http\Controllers\UserController::class, 'update'])->name('update');
         Route::delete('/{id}', [App\Http\Controllers\UserController::class, 'delete'])->name('delete');
+    });
+    Route::group(['prefix' => 'users', 'as' => 'users.'], function() {
+        Route::get('/', [App\Http\Controllers\UserController::class, 'getAll'])->name('index');
+        Route::post('/follow/{id}', [App\Http\Controllers\UserController::class, 'follow'])->name('follow');
+        Route::delete('/follow/{id}', [App\Http\Controllers\UserController::class, 'unfollow'])->name('unfollow');
+        Route::get('/follower', [App\Http\Controllers\UserController::class, 'getAllFollowers'])->name('getFollowers');
+        Route::get('/followed', [App\Http\Controllers\UserController::class, 'getFollowedUsers'])->name('getFollowedUsers');
     });
     Route::group(['prefix' => 'tweets', 'as' => 'tweet.'], function() {
         Route::post('/', [App\Http\Controllers\TweetController::class, 'createTweet'])->name('createTweet');
