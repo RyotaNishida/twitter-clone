ebbece23d4a253cbc9433481e6551f27deffa2cb
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-22 17:52:43 +0900

    [add]パッケージの追加

--- twitter/webpack.mix.js
+++ twitter/webpack.mix.js
@@ -11,7 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ // mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+mix.js('resources/js/app.js', 'public/js').vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        // postCss: [
+        //     require('postcss-import'),
+        //     require('tailwindcss'),
+        // ]
+    });
