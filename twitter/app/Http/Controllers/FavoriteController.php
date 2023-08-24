f2bd56d6035307e95a4b520ab3165ff276ae4eed
西田亮太 <nishidaryota@HajimariRyotaNishida.local>
2023-08-24 15:49:59 +0900

    [update]Controllerファイルの修正(まなてぃーさんレビュー箇所)

--- twitter/app/Http/Controllers/FavoriteController.php
+++ twitter/app/Http/Controllers/FavoriteController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
+use App\Models\Tweet;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Log;
-use App\Models\Tweet;
-use App\Models\Favorite;
-use App\Models\User;
 
 
 class FavoriteController extends Controller
@@ -18,14 +18,15 @@ class FavoriteController extends Controller
      * ツイートにいいねする
      *
      * @param Request $request
-     * @param favorite $favorite
+     * @param Favorite $favorite
+     * @param User $user
      * @return void
      */
-    public function favoriteTweet(Request $request, favorite $favorite)
+    public function favoriteTweet(Request $request, Favorite $favorite, User $user)
     {
         $userId = auth()->id(); // 現在のユーザーのIDを取得
         $tweetId = $request->input('tweetId'); // リクエストからツイートのIDを取得
-        $isFavorite = $favorite->isFavorite($userId, $tweetId); // ユーザーが特定のツイートをお気に入りに登録済みかどうかを確認
+        $isFavorite = $user->isFavorite($tweetId); // ユーザーが特定のツイートをお気に入りに登録済みかどうかを確認
         if(!$isFavorite) {
             $favorite->favorite($userId, $tweetId);
         } else {
@@ -36,16 +37,13 @@ class FavoriteController extends Controller
     /**
      * ログイン中のユーザーがいいねしたツイートを取得
      *
-     * @param Favorite $favorite
      * @param Tweet $tweet
-     * @return view
+     * @return View
      */
-    public function getAllByTweetIds(Favorite $favorite, Tweet $tweet): view
+    public function getAllByTweetIds(Tweet $tweet): View
     {
         $userId = auth()->id();
         $favoriteTweets = $tweet->getAllFavoriteTweets($userId);
-        return view('tweet.favorite', [
-            'favoriteTweets' => $favoriteTweets
-        ]);
+        return view('tweet.favorite', compact('favoriteTweets'));
     }
 }
